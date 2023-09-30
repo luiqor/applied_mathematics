bacb351c132f43e1ca534246535fe12d790e3e03
luiqor <vohnenna@gmail.com>
2023-09-30 21:55:40 +0300

    5th by teacher method (still only positive value)

--- lab_2/5_2.py
+++ lab_2/5_2.py
@@ -2,7 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-5, 5, 500)
-y1 = np.sqrt((1-(x**2/20))*5)
+y1=np.linspace(-10,10,60)
+x,y1 =np.meshgrid(x, y1)
+equation = (x**2/20)+(y1**2/5)
+
+plt.contour(x,y1,equation, levels=[1], colors="magenta")
+
 # y= (x**2/20)+(y**2/5)=1
 # y1_neg = -(np.sqrt((1-(x**2/20))*5))
 y2 = (6-12*x)/5
@@ -12,7 +17,6 @@ plt.xlabel("x")
 plt.ylabel("y1, y2")
 plt.grid()
 plt.plot(x, y1, label='y1 = np.sqrt((1-(x**2/20))*5)')
-# plt.plot(x, y1_neg, label='y1 = -np.sqrt((1-(x**2/20))*5)')
 plt.plot(x, y2, label='y2 = (6-12*x)/5')
 plt.legend()
 
