dba9d8a3f5d75dea085e543e568248ec2c008cc3
luiqor <vohnenna@gmail.com>
2023-10-01 21:36:10 +0300

    1_2 graph position fixed

--- lab_2/1_2 .py
+++ lab_2/1_2 .py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-t = np.linspace(np.pi , np.pi*2, 2000)
-y= 1/ ((np.cos(t))**2)
+import matplotlib.axes
+
+t = np.linspace(-np.pi , np.pi, 10000)
 x= np.log(np.tan(t))
+y= 1/ (np.cos(t)**2)
+# x= 5*np.sin(2*t)
+# y= 10*np.cos(2*t)
 #що таке ln, чи правильно я його задала - правильно, вкажи в лабі що log це для натурального як раз
 
 plt.figure(figsize=(9, 9))
@@ -12,5 +16,8 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Параметрично задана крива')
 plt.legend(['y= 1/ ((np.cos(t))**2), x= np.log(np.tan(t))'], loc='upper center')
+plt.axis([-10, 10, -2, 15])
+plt.axhline(0, color='black', linewidth=1)
+plt.axvline(0, color='black', linewidth=1)
 
 plt.show()
--- lab_2/2_2.py
+++ lab_2/2_2.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.axes as ax
-theta = np.linspace(0, np.pi*2, 9000000)
+theta = np.linspace(-np.pi, np.pi, 5000)
 ro = (3 / (1+ 2 * np.cos(theta)) )
-
 
 plt.figure(figsize=(6, 6))
 plt.polar(theta, ro, "r", label='ro = 3 / (1+ 2 * np.cos(fi))')
